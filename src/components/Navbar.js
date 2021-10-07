4bf6f6c6b8840af00a0574422e222c657a855376
Jack Pritom Soren <jackpritomsoren@hotmail.com>
2021-10-07 19:04:18 +0600

    useState

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,36 +11,95 @@ import {
 
 import { Cancel, Mail, Notifications, Search } from "@material-ui/icons";
 
-const useStyles = makeStyles((theme) => ({
-    logoLg: {
-        display: 'none',
-        [theme.breakpoints.up('sm')]: {
-            display: 'block',
-        }
+import { useState } from "react";
 
+const useStyles = makeStyles((theme) => ({
+    toolbar: {
+      display: "flex",
+      justifyContent: "space-between",
+    },
+    logoLg: {
+      display: "none",
+      [theme.breakpoints.up("sm")]: {
+        display: "block",
+      },
     },
     logoSm: {
-        display: 'block',
-        [theme.breakpoints.up('sm')]: {
-            display: 'none',
-        }
-    }
-}))
+      display: "block",
+      [theme.breakpoints.up("sm")]: {
+        display: "none",
+      },
+    },
+    search: {
+      display: "flex",
+      alignItems: "center",
+      backgroundColor: alpha(theme.palette.common.white, 0.15),
+      "&:hover": {
+        backgroundColor: alpha(theme.palette.common.white, 0.25),
+      },
+      borderRadius: theme.shape.borderRadius,
+      width: "50%",
+      [theme.breakpoints.down("sm")]: {
+        display: (props) => (props.open ? "flex" : "none"),
+        width: "70%",
+      },
+    },
+    input: {
+      color: "white",
+      marginLeft: theme.spacing(1),
+    },
+    cancel: {
+      [theme.breakpoints.up("sm")]: {
+        display: "none",
+      },
+    },
+    searchButton: {
+      marginRight: theme.spacing(2),
+      [theme.breakpoints.up("sm")]: {
+        display: "none",
+      },
+    },
+    icons: {
+      alignItems: "center",
+      display: (props) => (props.open ? "none" : "flex"),
+    },
+    badge: {
+      marginRight: theme.spacing(2),
+    },
+  }));
+  
 
 const Navbar = () => {
-    const classes = useStyles()
+    const [open, setOpen] = useState(false);
+    const classes = useStyles({open})
     return (
         <AppBar>
-            <Toolbar>
+            <Toolbar className={classes.toolbar}>
                 <Typography variant='h6' className={classes.logoLg}>
                     Jack
                 </Typography>
                 <Typography variant='h6' className={classes.logoSm}>
-                    SOREN
+                    JPS
                 </Typography>
                 <div className={classes.search}>
                     <Search />
-                    <InputBase placeholder="Search.." />
+                    <InputBase placeholder="Search.." className={classes.input} />
+                </div>
+                <div className={classes.icons}>
+                    <Search
+                        className={classes.searchButton}
+                        onClick={() => setOpen(true)}
+                    />
+                    <Badge badgeContent={4} color="secondary" className={classes.badge}>
+                        <Mail />
+                    </Badge>
+                    <Badge badgeContent={2} color="secondary" className={classes.badge}>
+                        <Notifications />
+                    </Badge>
+                    <Avatar
+                        alt="Remy Sharp"
+                        src="https://cdn-icons-png.flaticon.com/512/149/149071.png"
+                    />
                 </div>
             </Toolbar>
         </AppBar>
